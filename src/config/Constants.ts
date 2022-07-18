03bb973eb66b5241ebcf732e89d3481bfd34750f
João Carvalho <54593758+jovicarvalho@users.noreply.github.com>
2022-07-18 02:20:39 -0300

    Alteração da base de dados no Mongo DB
    Direcionando para o meu banco de dados

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+		'mongodb+srv://jovicarvalho:<password>@url-shortner-dio.9pwwfdv.mongodb.net/?retryWrites=true&w=majority',
 }
